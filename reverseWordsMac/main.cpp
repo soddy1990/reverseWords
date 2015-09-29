c34e23f0ae4ed5eddb6efdcea4c1681e53b962bf
soddy1990 <soddy1990@gmail.com>
2015-09-29 15:30:43 -0700

    Initial Commit

---
+++ reverseWordsMac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  reverseWordsMac
+//
+//  Created by 黄美强 on 15/9/29.
+//  Copyright (c) 2015年 黄 美强. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
